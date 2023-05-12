e80ef57cd64dafb5942cd0ff0d7ff20186939aab
Salem <mateusonego@gmail.com>
2023-05-12 16:51:26 -0300

    12/05

---
+++ JavaScript/JavaScripitLógica/Script.js
@@ -0,0 +1,7 @@
+function Concatenar() {
+
+        let nome = document.getElementById("pNome").value;
+        let sobrenome = document.getElementById("pSobre").value
+        alert(nome + " " + sobrenome);
+    
+}
