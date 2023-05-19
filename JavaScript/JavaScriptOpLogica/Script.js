e61d3e25a5029cd7d41b4359545dd42f6c2e9f24
Salem <mateusonego@gmail.com>
2023-05-19 15:02:10 -0300

    a

---
+++ JavaScript/JavaScriptOpLogica/Script.js
@@ -0,0 +1,12 @@
+
+function nParImpar() {
+    let num = document.getElementById("nDigitado").value;
+
+    if (num % 2 == 0) {
+        alert("Seu número é par")
+
+    } else {
+        alert("Seu número é impar")
+    }
+
+}
