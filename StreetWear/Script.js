1197f89088452b8129660b1cc789f3240f273966
salleeem <mateusonego@gamil.com>
2023-08-16 16:48:30 -0300

    cu

--- StreetWear/Script.js
+++ StreetWear/Script.js
@@ -8,3 +8,34 @@ $('.navTrigger').click(function () {
 
 
 //Não mexer!!!!!!!
+
+
+const carousel = document.querySelector('.carousel');
+const cards = document.querySelectorAll('.card');
+const cardWidth = cards[0].offsetWidth;
+const prevButton = document.querySelector('.prev-btn');
+const nextButton = document.querySelector('.next-btn');
+
+let currentPosition = 0;
+
+prevButton.addEventListener('click', () => {
+  if (currentPosition > 0) {
+    currentPosition--;
+    updateCarousel();
+  }
+});
+
+nextButton.addEventListener('click', () => {
+  if (currentPosition < cards.length - 1) {
+    currentPosition++;
+    updateCarousel();
+  }
+});
+
+function updateCarousel() {
+  const translateXValue = -currentPosition * cardWidth;
+  carousel.style.transform = `translateX(${translateXValue}px)`;
+}
+
+// Inicializar o carrossel
+updateCarousel();
